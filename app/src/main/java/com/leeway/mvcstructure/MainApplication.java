781e3bba135629ba9500f9877fe27b649dfcb935
Nutti Saelor <leelorz6@gmail.com>
2016-12-27 01:51:22 +0700

    add Application and Folders

---
+++ app/src/main/java/com/leeway/mvcstructure/MainApplication.java
@@ -0,0 +1,22 @@
+package com.leeway.mvcstructure;
+
+import android.app.Application;
+
+/**
+ * Created by Lee Lorz on 12/27/2016.
+ */
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // initialize thing(s) here
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
--- app/src/main/java/com/leeway/mvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/leeway/mvcstructure/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.leeway.mvcstructure;
+package com.leeway.mvcstructure.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.leeway.mvcstructure.R;
 
 public class MainActivity extends AppCompatActivity {
 
