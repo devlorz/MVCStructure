36562f02f9c7cd1b5e04a802320f71dc11cdc6f5
Nutti Saelor <leelorz6@gmail.com>
2017-01-03 03:06:55 +0700

    add Contextor and Singleton

--- app/src/main/java/com/leeway/mvcstructure/MainApplication.java
+++ app/src/main/java/com/leeway/mvcstructure/MainApplication.java
@@ -2,6 +2,8 @@ package com.leeway.mvcstructure;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by Lee Lorz on 12/27/2016.
  */
@@ -13,6 +15,7 @@ public class MainApplication extends Application {
         super.onCreate();
 
         // initialize thing(s) here
+        Contextor.getInstance().init(getApplicationContext());
     }
 
     @Override
--- app/src/main/java/com/leeway/mvcstructure/activity/MainActivity.java
+++ app/src/main/java/com/leeway/mvcstructure/activity/MainActivity.java
@@ -2,8 +2,10 @@ package com.leeway.mvcstructure.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.leeway.mvcstructure.R;
+import com.leeway.mvcstructure.util.ScreenUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int screenWidth = ScreenUtils.getInstance().getScreenWidth();
+        int screenHeight = ScreenUtils.getInstance().getScreenHeight();
+
+        Toast.makeText(MainActivity.this,
+                "Width = " + screenWidth + " Height = " + screenHeight,
+                Toast.LENGTH_SHORT).show();
     }
 }
