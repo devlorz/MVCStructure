f4931e25db825ca8c4478e4874311b862dc5a583
Nutti Saelor <leelorz6@gmail.com>
2017-01-07 02:42:30 +0700

    add button to second fragment

--- app/src/main/java/com/leeway/mvcstructure/fragment/SecondFragment.java
+++ app/src/main/java/com/leeway/mvcstructure/fragment/SecondFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.leeway.mvcstructure.R;
 
@@ -13,6 +14,8 @@ import com.leeway.mvcstructure.R;
  * Created by nuuneoi on 11/16/2014.
  */
 public class SecondFragment extends Fragment {
+
+    Button btnOK;
 
     public SecondFragment() {
         super();
@@ -35,6 +38,13 @@ public class SecondFragment extends Fragment {
 
     private void initInstances(View rootView) {
         // Init 'View' instance(s) with rootView.findViewById here
+        btnOK = (Button) rootView.findViewById(R.id.btnOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().popBackStack();
+            }
+        });
     }
 
     @Override
