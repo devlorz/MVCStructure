0ad4476d8b66b5a9893522cc4aaf8e3961a2c7e0
Nutti Saelor <leelorz6@gmail.com>
2017-01-08 16:57:27 +0700

    add Crashlytics

--- app/src/main/java/com/leeway/mvcstructure/MainApplication.java
+++ app/src/main/java/com/leeway/mvcstructure/MainApplication.java
@@ -3,6 +3,8 @@ package com.leeway.mvcstructure;
 import android.app.Application;
 
 import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Lee Lorz on 12/27/2016.
@@ -13,6 +15,11 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
 
         // initialize thing(s) here
         Contextor.getInstance().init(getApplicationContext());
@@ -22,4 +29,13 @@ public class MainApplication extends Application {
     public void onTerminate() {
         super.onTerminate();
     }
+
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345");
+        Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("Test User");
+    }
+
 }
